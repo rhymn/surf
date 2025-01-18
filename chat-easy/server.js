3043e95285bcbae4ac4d10246a0684de4551055e
rhymn <david@pnd.se>
2025-01-18 19:12:28 +0100

    port

--- chat-easy/server.js
+++ chat-easy/server.js
@@ -6,7 +6,7 @@ const socketIo = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 // Database connection details
 const db = pgp('postgres://username:password@localhost:5432/mydatabase');
