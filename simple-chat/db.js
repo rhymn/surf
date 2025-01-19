476cf20db4124d9b80a8384937a29ca8b4d21052
rhymn <david@pnd.se>
2025-01-19 14:46:14 +0100

    db

--- simple-chat/db.js
+++ simple-chat/db.js
@@ -1,6 +1,6 @@
 const pgp = require('pg-promise')();
-const databaseUrl = process.env.DATABASE_URL || 'postgresql://private_il06_user:XmeLpBF41nXXiB6SQeEiXehcrOTlzVRq@dpg-cu6en456l47c73c0fr6g-a.frankfurt-postgres.render.com/private_il06';
-const db = pgp(`${databaseUrl}?ssl=true`);
+const databaseUrl = process.env.DATABASE_URL || 'postgresql://private_il06_user:XmeLpBF41nXXiB6SQeEiXehcrOTlzVRq@dpg-cu6en456l47c73c0fr6g-a.frankfurt-postgres.render.com/private_il06' + '?ssl=true';
+const db = pgp(databaseUrl);
 
 // Create messages table if it doesn't exist
 db.none(`
