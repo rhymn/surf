cbfd20972af3c58511e26c1e7ea19ae94b04e17d
rhymn <david@pnd.se>
2025-01-19 14:00:16 +0100

    save and restore in db

--- simple-chat/server.js
+++ simple-chat/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-// const { saveMessage, getMessages } = require('./db');
+const { saveMessage, getMessages } = require('./db');
 
 const app = express();
 const server = http.createServer(app);
@@ -28,11 +28,11 @@ io.on('connection', (socket) => {
     console.log(`User joined room: ${room}`);
 
     // Fetch and send previous messages to the new user
-    // getMessages(room).then(messages => {
-    //     socket.emit('previousMessages', messages);
-    // }).catch(error => {
-    //     console.error('Error fetching messages:', error);
-    // });
+    getMessages(room).then(messages => {
+        socket.emit('previousMessages', messages);
+    }).catch(error => {
+        console.error('Error fetching messages:', error);
+    });
 
     // Handle new user
     socket.on('newUser', (userName) => {
@@ -42,18 +42,18 @@ io.on('connection', (socket) => {
 
     // Handle chat messages
     socket.on('message', (msg) => {
-        const userName = users[socket.id];
-        console.log(`Message received: ${msg}`);
+        const {username, message} = msg;
+        console.log(`Message received from ${username}: ${message}`);
         io.to(room).emit('message', msg);
-        // saveMessage(room, userName, msg).catch(error => {
-        //     console.error('Error saving message:', error);
-        // });
+        saveMessage(room, username, message).then(console.log('Message stored')).catch(error => {
+            console.error('Error saving message:', error);
+        });
     });
 
     socket.on('disconnect', () => {
-        const userName = users[socket.id];
+        const username = users[socket.id];
         delete users[socket.id];
-        io.to(room).emit('message', `${userName} left the chat`);
+        io.to(room).emit('message', `${username} left the chat`);
         console.log('A user disconnected');
     });
 });
